e6e58db58694d0afc6d74067eb6072667b7a480d
Daniel Medeiros <danielmmf@gmail.com>
2017-07-03 01:24:59 -0300

    esvaziando o readme

--- index.php
+++ index.php
@@ -40,7 +40,26 @@ Message:
     <div id="main" >
         
     </div>
- 
+ <script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.6.1/angular.min.js"></script>
+
+<!-- Firebase -->
+<script src="https://www.gstatic.com/firebasejs/3.6.6/firebase.js"></script>
+
+<!-- AngularFire -->
+<script src="https://cdn.firebase.com/libs/angularfire/2.3.0/angularfire.min.js"></script>
+
+<script>
+  // Initialize the Firebase SDK
+  var config = {
+     apiKey: "AIzaSyBlZVMRe38jJfrwead1evIuV3OfIEVXlCY",
+    authDomain: "truco-b2e65.firebaseapp.com",
+    databaseURL: "https://truco-b2e65.firebaseio.com",
+    projectId: "truco-b2e65",
+    storageBucket: "",
+    messagingSenderId: "680811678214"
+  };
+  firebase.initializeApp(config);
+</script>
 
 	<script type="text/javascript" src="js/jquery.min.js"></script>
     <script type="text/javascript" src="js/raphael-min.js"></script> <!-- Raphael 1.5.2 -->
@@ -99,8 +118,31 @@ Message:
 	}
 
 
-		   
+
+
+
+var app = angular.module("sampleApp", ["firebase"]);
+app.controller("SampleCtrl", function($scope, $firebaseArray) {
+  var ref = firebase.database().ref().child("messages");
+  // create a synchronized array
+  $scope.messages = $firebaseArray(ref);
+  // add new items to the array
+  // the message is automatically added to our Firebase database!
+  $scope.addMessage = function() {
+    $scope.messages.$add({
+      text: $scope.newMessageText
+    });
+  };
+  // click on `index.html` above to see $remove() and $save() in action
+});   
     </script>
+
+  <div ng-controller="SampleCtrl">
+    <ul>
+      <li ng-repeat="message in messages">{{ message.text }}</li>
+    </ul>
+  </div>
+
 	
 </body>
 </html>
